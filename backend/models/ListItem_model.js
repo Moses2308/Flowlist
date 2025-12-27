fad48048c640ed7edb057fb4dc06ac7d32a1ba3a
Moses Ventura <moses2308ventura@gmail.com>
2025-12-27 10:33:51 -0800

    Defined ListItem model and its relationship to the List Model. Additionally, updated  the reset db function and added comments

--- backend/models/ListItem_model.js
+++ backend/models/ListItem_model.js
@@ -1,27 +1,26 @@
 import sequelizeConn from "../config/dbconfig.js";
 import { Model, DataTypes } from "sequelize";
 
-class List extends Model {}
-List.init(
+class ListItem extends Model {}
+ListItem.init(
   {
     id: {
       primaryKey: true,
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
     },
     title: {
       type: DataTypes.STRING(64),
-      defaultValue: "Untitled List",
-      allowNull: true, // might need to change this in the future
+      defaultValue: "untitled task",
+      allowNull: true, //might need to change this in the future
     },
   },
   {
     sequelize: sequelizeConn,
-    modelName: "List",
+    modelName: "ListItem",
     timestamps: true,
     paranoid: true, //look into cron jobs to periodically delete "deleted" items
   }
 );
 
-export default List;
+export default ListItem;
--- backend/models/User_model.js
+++ backend/models/User_model.js
@@ -2,6 +2,7 @@ import sequelizeConn from "../config/dbconfig.js";
 import { Model, DataTypes } from "sequelize";
 import bcrypt from "bcrypt";
 import List from "./List_model.js";
+import ListItem from "./ListItem_model.js";
 
 class User extends Model {}
 User.init(
@@ -30,10 +31,12 @@ User.init(
   }
 );
 
+//dev function to help test db definition.
 resetDatabase();
 
 export default User;
 
+//Todo: Complete the hook to salt user password on password creation
 async function beforeValidate(user, options) {
   if (!user.password_hash) {
     user.password_hash = await bcrypt.hash(user.plain_password, 13);
@@ -41,18 +44,28 @@ async function beforeValidate(user, options) {
 }
 
 async function resetDatabase() {
+  //attempting to drop all tables
   try {
+    await ListItem.drop();
     await List.drop();
     await User.drop();
   } catch (error) {
     console.log("something went wrong dropping the tables", error.message);
   }
 
+  // Association between User and its Lists (1:M)
   User.hasMany(List);
   List.belongsTo(User);
 
+  // Association between List and its List items (1:M)
+  List.hasMany(ListItem);
+  ListItem.belongsTo(List);
+
+  //creating tables in database, and dropping if they already exist (they shouldn't because of the earlier drops)
   await User.sync({ force: true });
   await List.sync({ force: true });
+  await ListItem.sync({ force: true });
 
+  //message to confirm success of the script.
   console.log("database reset");
 }
