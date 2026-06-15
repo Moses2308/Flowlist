0b6dff89fe5cebc4a2999244bcff3150cabe85fe
Moses Ventura <moses2308ventura@gmail.com>
2026-06-15 11:55:25 -0700

    FIX: sequelize connection variable renamed & env log removed

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -2,15 +2,13 @@ import Sequelize from "sequelize";
 
 const { DBUSERNAME, DBPASSWORD, DBHOST, DBDIALECT } = process.env;
 
-console.log(DBUSERNAME, DBPASSWORD, DBHOST, DBDIALECT);
-
 const sequelizeConn = new Sequelize("flowlist", DBUSERNAME, DBPASSWORD, {
   host: DBHOST,
   dialect: "mysql",
 });
 
 try {
-  await sequelize.authenticate();
+  await sequelizeConn.authenticate();
   console.log("connection successful");
 } catch (error) {
   console.log("error connecting to database", error.message);
