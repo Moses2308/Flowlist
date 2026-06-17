400d0d2c2a0eebcc25ec47ea21ae6dd7527f0397
Moses Ventura <moses2308ventura@gmail.com>
2026-06-16 19:25:02 -0700

    FIX/FEAT: Updated Users' Model hook to facilitate password updates & added instance methods to streamline field manipulation

--- backend/models/UsersModel.js
+++ backend/models/UsersModel.js
@@ -2,7 +2,43 @@ import { Model, DataTypes } from "sequelize";
 import bcrypt from "bcrypt";
 
 export default function defineUsers(sequelizeConn) {
-  class Users extends Model {}
+  class Users extends Model {
+    //instance method to update fields
+    patchFields(fieldsObj) {
+      const fields = Object.entries(fieldsObj);
+      const fieldsUpdated = [];
+
+      fields.forEach((field) => {
+        switch (field[0]) {
+          case "password":
+            this.rawPassword = field[1];
+            fieldsUpdated.push(field[0]);
+            break;
+          case "email":
+            this.email = field[1];
+            fieldsUpdated.push(field[0]);
+            break;
+          default:
+            break;
+        }
+      });
+
+      return fieldsUpdated;
+    }
+
+    //instance method to delete password hash before resposne
+    deletePassHash() {
+      delete this.dataValues.passwordHash;
+      return this;
+    }
+
+    //instance method to delete password before response
+    deleteRawPassword() {
+      delete this.dataValues.rawPassword;
+      return this;
+    }
+  }
+
   Users.init(
     {
       id: {
@@ -23,16 +59,23 @@ export default function defineUsers(sequelizeConn) {
       },
       passwordHash: {
         allowNull: false,
-        type: DataTypes.STRING(60), //front end validation
+        //TODO: notify users of this restriction through frontend
+        type: DataTypes.STRING(60),
       },
     },
     {
       sequelize: sequelizeConn,
       tableName: "users",
+      /*
+        TODO: Refactor hooks to use delete instance methods to delete sensitive
+        data on save.
+      */
       hooks: {
         beforeValidate: async (user, options) => {
-          user.passwordHash = await bcrypt.hash(user.rawPassword, 15);
-          user.rawPassword = null;
+          if (user.rawPassword) {
+            user.passwordHash = await bcrypt.hash(user.rawPassword, 15);
+            user.rawPassword = null;
+          }
         },
       },
     },
